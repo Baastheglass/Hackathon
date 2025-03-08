b5b2b1612fde0475d8e1233a11bd72dc84f763e3
Baastheglass <lightningblazer123@gmail.com>
2025-03-08 12:43:15 +0500

    preprocessing

---
+++ code/preprocessing.py
@@ -0,0 +1,8 @@
+import os
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+
